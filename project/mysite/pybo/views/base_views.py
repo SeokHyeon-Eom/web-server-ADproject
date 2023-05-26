bf44c6aa9488f5a3d8dfd8a587807967474b63f9
SeokHyeon-Eom <eom2244@kookmin.ac.kr>
2023-05-27 00:26:41 +0900

    add: sort page

--- project/mysite/pybo/views/base_views.py
+++ project/mysite/pybo/views/base_views.py
@@ -1,13 +1,21 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
+from django.db.models import Q, Count
 from pybo.models import Question, Answer, Comment
 
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
-    question_list = Question.objects.order_by('-create_date')
+    so = request.GET.get('so', 'recent')  # 정렬
+
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:
+        question_list = Question.objects.order_by('-create_date')
+    
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |  # 제목
@@ -18,7 +26,7 @@ def index(request):
         ).distinct()
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
 
